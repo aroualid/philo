abbec7dc4efa4e796d5e46446fc4fe67bb501523
aroualid <aroualid@student.42.fr>
2024-09-18 18:39:48 +0200

    je crois j'ai mieux compris

--- philo.h
+++ philo.h
@@ -1,28 +1,33 @@
 #ifndef PHILO_H
 # define PHILO_H
 
+typedef struct s_args t_args;
+
 # include "stdio.h"
 # include <limits.h>
 # include <stdlib.h>
 # include <threads.h>
 # include <pthread.h>
 
-typedef struct s_args
+typedef struct s_philo
 {
-	int					nb_philo;
 	int					time_to_die;
 	int					time_to_eat;
 	int					time_to_sleep;
 	int					each_eat;
-	int					philo_n;				
+	int					philo_nb;
+	pthread_mutex_t		left_fork;
+	pthread_mutex_t		right_fork;
+	t_args				*args;
+}						t_philo;
+
+typedef struct s_args
+{
+	int					nb_philo;
+	int					philo_n;
 	pthread_mutex_t		mutex;
-
+	t_philo				**philo;
 }						t_args;
-
-typedef struct s_philo {
-    int philo_n;
-    t_args *args;
-} t_philo;
 
 long	ft_atol(const char *str);
 int		ft_strlen(char *s);
--- src/main.c
+++ src/main.c
@@ -1,22 +1,60 @@
 #include "../philo.h"
+#include <bits/pthreadtypes.h>
+#include <pthread.h>
 #include <strings.h>
 
-void	fill_value(int i, long atol, t_args *args)
+
+int		create_fork(t_args *args)
 {
-	if (i == 1)
-		args->nb_philo = atol;
-	if (i == 2)
-		args->time_to_die = atol;
-	if (i == 3)
-		args->time_to_eat = atol;
-	if (i == 4)
-		args->time_to_sleep = atol;
-	if (i == 5)
-		args->each_eat = atol;
+	int				i;
+	pthread_mutex_t *forks;
+	
+	i = 0;
+	forks = calloc(sizeof(pthread_mutex_t), args->nb_philo);
+	while (i < args->nb_philo)
+	{
+		if (pthread_mutex_init(&forks[i], NULL) == -1)
+			return(1);
+		i++;
+	}
+	i = 0;
+	while (i < args->nb_philo)
+	{
+		args->philo[i]->left_fork = forks[i];
+		if (i != 0)
+			args->philo[i]->right_fork = forks[i -1];
+		else if (i == 0)
+			args->philo[i]->right_fork = forks[args->nb_philo - 1];
+		i++;
+	}
+	return (0);
 }
 
 
-int	check_arg(char **av, int ac, t_args *args)
+void	fill_value(char **av, t_args *args, int ac)
+{
+	int	i;
+
+	i = 0;
+	args->nb_philo = ft_atol(av[1]);
+	args->philo = calloc(sizeof(t_philo *), args->nb_philo);
+	while (i < args->nb_philo)
+	{
+		args->philo[i]->philo_nb = i + 1;
+		args->philo[i] = calloc (sizeof(t_philo), 1I);
+		args->philo[i]->time_to_die = ft_atol(av[2]);
+		args->philo[i]->time_to_eat = ft_atol(av[3]);
+		args->philo[i]->time_to_sleep = ft_atol(av[4]);
+		if (ac == 6)
+			args->philo[i]->each_eat = ft_atol(av[5]);
+		else
+			args->philo[i]->each_eat = -1;
+		i++;
+	}
+	create_fork(args);
+}
+
+int	check_arg(char **av, int ac)
 {
 	int		i;
 	long	atol;
@@ -37,8 +75,6 @@ int	check_arg(char **av, int ac, t_args *args)
 		atol = ft_atol(av[i]);
 		if ((atol > INT_MAX || atol <= 0) || (ft_strlen(av[i]) >= 11))
 			return (0);
-		else
-			fill_value (i, atol, args);
 		j = 0;
 		i++;
 	}
@@ -46,73 +82,13 @@ int	check_arg(char **av, int ac, t_args *args)
 }
 
 
-void	*print_test_odd(void *arg)
-{
-	t_args *args = (t_args *)arg;
-	int	i = args->philo_n;
-	
-	if (i % 2 == 0)
-	{
-		pthread_mutex_lock(&args->mutex);
-	    printf("%i est pair\n", i);
-	    pthread_mutex_unlock(&args->mutex); 
-	}
-	else
-	{
-		pthread_mutex_lock(&args->mutex);
-	    printf("%i est impair\n", i);
-	    pthread_mutex_unlock(&args->mutex); 
-	}
-	return NULL;
-}
-
-
-int	test_thread(t_args *args)
-{
-	pthread_t	thread[args->nb_philo];
-	int	i = 0;
-
-	pthread_mutex_init(&args->mutex, NULL);
-
-	printf("nb = %i\n", args->nb_philo);
-	while (i < args->nb_philo)
-	{
-		pthread_mutex_lock(&args->mutex);
-		args->philo_n = i + 1;
-		pthread_mutex_unlock(&args->mutex);
-		
-		if (pthread_create(&thread[i], NULL, print_test_odd, args)) 
-		{
-			fprintf(stderr, "Erreur lors de la création du thread\n");
-			return 1;
-		}
-        i++;
-    }
-	for (int j = 0; j < args->nb_philo; j++)
-        pthread_join(thread[j], NULL);
-	pthread_mutex_destroy(&args->mutex);
-	return (0);
-}
-
 int main (int ac, char **av)
 {
-	t_args		*args;
+	t_args			args;
 
-	args = calloc(sizeof(t_args) + sizeof(pthread_mutex_t), 1);
 	if (ac == 5 || ac == 6)
 	{
-		if (check_arg(av, ac, args) == 0)
-		{
-			free (args);
-			printf("args error\n");
-			return (-1);
-		}
-		else
-		{
-			test_thread(args);
-		}
+		if (check_arg(av, ac) == 1)
+			fill_value(av, &args, ac);
 	}
-	else
-		printf ("non\n");
-	free (args);
 }
